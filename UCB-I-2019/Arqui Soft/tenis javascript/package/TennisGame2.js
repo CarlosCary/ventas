9478717bec0c9c49224a04a00615e2425e36d46f
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-02-18 11:51:10 -0400

    Refactor logic in a method

--- UCB-I-2019/Arqui Soft/tenis javascript/package/TennisGame2.js
+++ UCB-I-2019/Arqui Soft/tenis javascript/package/TennisGame2.js
@@ -9,6 +9,41 @@ var TennisGame2 = function(player1Name, player2Name) {
     this.player2Name = player2Name;
 };
 
+TennisGame2.prototype.getScore = function() {
+    let result;
+
+    this.Player1Result = assignGenericResult(this.Player1Points);
+    this.Player2Result = assignGenericResult(this.Player2Points);
+
+    if(this.player1AndPlayer2HaveLess3PointsAndAreDraw()) {
+        result = this.Player1Result + "-" + "All";
+        return result;
+    }
+    
+    if(this.player1AndPlayer2HaveGreater3PointsAndAreDraw()) {
+        return "Deuce";
+    }
+
+    result = this.Player1Result + "-" + this.Player2Result;
+
+    if (this.player1HaveAdvantage()) {
+        result = "Advantage player1";
+    }
+
+    if (this.player2HaveAdvantage()) {
+        result = "Advantage player2";
+    }
+
+    if (this.player1Win()) {
+        result = "Win for player1";
+    }
+    if (this.Player2Points >= 4 && this.Player1Points >= 0 && (this.Player2Points - this.Player1Points) >= 2) {
+        result = "Win for player2";
+    }
+
+    return result;
+};
+
 TennisGame2.prototype.player1AndPlayer2HaveLess3PointsAndAreDraw = function() {
     return this.Player1Points === this.Player2Points && this.Player1Points < 3;
 }
@@ -18,7 +53,19 @@ TennisGame2.prototype.player1AndPlayer2HaveGreater3PointsAndAreDraw = function()
 }
 
 TennisGame2.prototype.player1HavePointsAndPlayer2HaveCeroPoints = function() {
-    return this.Player1Points > 0 && this.Player2Points === 0
+    return this.Player1Points > 0 && this.Player2Points === 0;
+}
+
+TennisGame2.prototype.player1HaveAdvantage = function() {
+    return this.Player1Points > this.Player2Points && this.Player2Points >= 3;
+}
+
+TennisGame2.prototype.player2HaveAdvantage = function() {
+    return this.Player2Points > this.Player1Points && this.Player1Points >= 3;
+}
+
+TennisGame2.prototype.player1Win = function() {
+    return this.Player1Points >= 4 && this.Player2Points >= 0 && (this.Player1Points - this.Player2Points) >= 2;
 }
 function assignGenericResult(playerPoints) {
     let result = "";
@@ -38,40 +85,6 @@ function assignGenericResult(playerPoints) {
     }
     return result;
 }
-TennisGame2.prototype.getScore = function() {
-    var result = "";
-
-    this.Player1Result = assignGenericResult(this.Player1Points);
-    this.Player2Result = assignGenericResult(this.Player2Points);
-
-    if(this.player1AndPlayer2HaveLess3PointsAndAreDraw()) {
-        result = this.Player1Result + "-" + "All";
-        return result;
-    }
-    
-    if(this.player1AndPlayer2HaveGreater3PointsAndAreDraw()) {
-        return "Deuce";
-    }
-
-    result = this.Player1Result + "-" + this.Player2Result;
-
-    if (this.Player1Points > this.Player2Points && this.Player2Points >= 3) {
-        result = "Advantage player1";
-    }
-
-    if (this.Player2Points > this.Player1Points && this.Player1Points >= 3) {
-        result = "Advantage player2";
-    }
-
-    if (this.Player1Points >= 4 && this.Player2Points >= 0 && (this.Player1Points - this.Player2Points) >= 2) {
-        result = "Win for player1";
-    }
-    if (this.Player2Points >= 4 && this.Player1Points >= 0 && (this.Player2Points - this.Player1Points) >= 2) {
-        result = "Win for player2";
-    }
-
-    return result;
-};
 
 TennisGame2.prototype.P1Score = function() {
     this.Player1Points++;
